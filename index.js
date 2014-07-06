fd657d9517be5e5978b7e322530c27e53b839a8d
Ilya Kantor <iliakan@gmail.com>
2014-07-06 12:21:14 +0400

    expose parser outside of the package

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.consts = require('./consts');
+exports.node = require('./node');
+
+exports.BodyParser = require('./parser/bodyParser').BodyParser;
+
+exports.HtmlTransformer = require('./transformer/htmlTransformer').HtmlTransformer;
+
+
+
