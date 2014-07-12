a89dbd83fd48311ee1b903d9ad8960dcae6f5390
Ilya Kantor <iliakan@gmail.com>
2014-07-12 09:29:17 +0400

    only lint changed files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp   = require('gulp');
-var gulpTaskLint = require('javascript-gulp-task-lint');
+const gulp   = require('gulp');
+const gulpTaskLint = require('javascript-gulp-task-lint');
+const execSync = require('child_process').execSync;
 
-gulp.task('lint', gulpTaskLint(['**/*.js', '!node_modules/**']));
+gulp.task('lint', function(cb) {
+  const files = execSync("git ls-files -m '*.js'", {encoding: 'utf-8'}).trim().split("\n");
+  gulpTaskLint(files)(cb);
+});
 
 gulp.task('pre-commit', ['lint']);
+
